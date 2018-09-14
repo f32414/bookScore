f3c4156cb7144045ea4a23995ec91fb97bdcdc33
elvis <oilgame@yeah.net>
2018-09-14 21:11:19 +0800

    20180914

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # bookScore 书籍评分系统
 > 始于 2018.09.14
-> 本项目基于Thinkphp5.1开发
 
 开发日志位于 ×Diary× 目录下
 
 版权归 &copy;f32414 所有
+
+> 本项目基于Thinkphp5.1开发
--- config/app.php
+++ config/app.php
@@ -15,11 +15,11 @@
 
 return [
     // 应用名称
-    'app_name'               => '',
+    'app_name'               => '书籍评分系统',
     // 应用地址
     'app_host'               => '',
     // 应用调试模式
-    'app_debug'              => false,
+    'app_debug'              => true,
     // 应用Trace
     'app_trace'              => false,
     // 是否支持多模块
@@ -54,7 +54,7 @@ return [
     // +----------------------------------------------------------------------
 
     // 默认模块名
-    'default_module'         => 'index',
+    'default_module'         => 'home',
     // 禁止访问模块
     'deny_module_list'       => ['common'],
     // 默认控制器名
